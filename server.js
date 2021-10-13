c551c98f9b3d419addd0833d58e40ec445b34fa3
Michael Cavalcanti <michaelmlcavalcanti@gmail.com>
2021-10-13 16:46:47 -0300

    Fim da segunda aula

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 const express = require('express');
 const app = express()
 
-const PORT = 3080
+app.get('/', (req, res) => {res.send({'sucess': true})})
 
-app.get('/', (req, res) =>{
-    res.json({
-        'sucess': true
-    })
-})
-
-app.get('/portfolio', (req, res) => {
-
-    const data = [
+app.get('/portfolio', 
+        (req, res) => 
+        {
+        const data = [
         {id: 1, name: "Meu primeiro projeto", createdAt: "13-10-2021"},
         {id: 2, name: "Meu segundo projeto", createdAt: "13-10-2021"},
         {id: 3, name: "Meu terceiro projeto", createdAt: "13-10-2021"}
-    ];
-
-    res.json({sucess: true, data: data});
-}
+        ];
+        res.send({sucess: true, data: data});
+    }
 )
 
+const PORT = 3080
 app.listen(PORT);
