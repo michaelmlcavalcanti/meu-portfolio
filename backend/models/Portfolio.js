13eaca4160098055b01421a828c8cb962ecfdc10
Michael Cavalcanti <michaelmlcavalcanti@gmail.com>
2021-10-16 13:14:50 -0300

    Criando o Schema Portfolio.

---
+++ backend/models/Portfolio.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const potfolioSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description : {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now()
+        }
+    }
+);
+
+module.exports = mongoose.model('portfolio', portfolioSchema)
