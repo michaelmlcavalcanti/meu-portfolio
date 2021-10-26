5ccae5cc69d0dfb1106856fb3e82b22a475d98d8
Michael Cavalcanti <michaelmlcavalcanti@gmail.com>
2021-10-26 17:25:00 -0300

    Desfazendo o deploy com frontend

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
-const express = require('express')
+const express = require('express');
 
-const api = require('./backend/routes')
+const api = require('./backend/routes');
 
-require('dotenv').config()
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+require('dotenv').config();
 
-app.use('/api', api)
+app.get('/', (req, res) => {res.send({'sucess': true})});
 
-if (process.env.NODE_ENV === 'production'){
-    //Express vai entregar os assets de produção
-    //Como por exemplo: main.js ou o main.css
-    api.use(express.static('frontend/build'))
+app.use('/api', api); 
 
-    //Express vai entregar o index.html, se não reconhecer a rota
-    const path = require('path')
-    app.get('*', function(req,res){
-        //__dirname/frontend/build/index.html
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    })
-}
-
-const PORT = process.env.PORT
-app.listen(PORT)
+const PORT = process.env.PORT;
+app.listen(PORT);
