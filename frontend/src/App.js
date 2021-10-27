7c92a29e0d37692f6c6faddd9fbf844bce6a97a6
Michael Cavalcanti <michaelmlcavalcanti@gmail.com>
2021-10-27 20:03:09 -0300

    Trocando pro frontend criado por mim.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,43 @@
-import React from 'react'
-
-//Global estilo
-import GlobalStyle from './components/GlobalStyle';
+import React from 'react';
 
 //Roteamento
-import { Route, Switch } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
 
 //Componentes
-import AboutUs from './pages/AboutUs';
-import Portfolio from './pages/Portfolio';
-import ContactMe from './pages/ContactMe';
-import Nav from './components/Nav';
+import Menu from './Componentes/Componente_Menu';
+import PaginaCurriculo from './Paginas/Pagina_Curriculo';
+import PaginaContato from './Paginas/Pagina_Contato';
+import PaginaInicial from './Paginas/Pagina_Inicial';
+import Tecnologias from './Componentes/Componente_Tecnologias';
+import { Secao1, Secao2, Secao3 } from './Estilo/Estilo_Global';
 
 function App() {
   return (
     <div>
-      <GlobalStyle />
-      <Nav />
-      <Switch>
-        <Route exact path='/'>
-          <AboutUs />
-        </Route>
-        <Route path='/portfolio'>
-          <Portfolio />
-        </Route>
-        <Route path='/contato'>
-          <ContactMe />
-        </Route>
-      </Switch>
+      <Secao1>
+        <Menu />
+      </Secao1>
+      <Secao2>
+        <Switch>
+            <Route exact path='/'>
+              <PaginaInicial />
+            </Route>
+            <Route path='/inicial'>
+              <PaginaInicial />
+            </Route>
+            <Route path='/curriculo'>
+              <PaginaCurriculo />
+            </Route>
+            <Route path='/contato'>
+              <PaginaContato />
+            </Route>
+          </Switch>
+      </Secao2>
+      <Secao3>
+        <Tecnologias />
+      </Secao3>
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/pages/Portfolio.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const Portfolio = function(){
-    return(<div>Portfolio</div>)}
-
-export default Portfolio
